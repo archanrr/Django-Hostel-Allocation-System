5b9a10a962bbcb8418de9bc05d591eec5029422c
Archan Rajaram R <archan.rajaram1@gmail.com>
2018-10-07 14:31:35 +0530

    path commit

--- forestfire/Hostel/forestfire/urls.py
+++ forestfire/Hostel/forestfire/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 urlpatterns = [
     #path('',include('polls.urls'))
     path('admin/', admin.site.urls),
-    path('login/', include('login.urls'))
+    path('', include('login.urls'))
 ]
--- forestfire/Hostel/login/urls.py
+++ forestfire/Hostel/login/urls.py
@@ -7,9 +7,9 @@ urlpatterns=[
     path('',views.StudentHostelAllocation,name='StudentHostelAllocation'),
     path('adminlogin/',views.adminlogin,name='adminlogin'),
     path('adminlogin/register/',views.register_page,name='register_page'),
+    path('adminlogin/register/signin/',views.signin,name='signin'),
     path('Studentlogin/',views.Studentlogin,name='Studentlogin'),
     path('Studentlogin/login_user/',views.login_user,name='login user'),
-    path('adminlogin/register/signin/',views.signin,name='signin'),
     path('Studentlogin/login_user/hostelAllocation/',views.hostelAllocation,name='hostelAllocation'),
     
 ]
